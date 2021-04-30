2359d247b1c29f46a3d03663b23c3808d3bd2ce4
waleed-ops <wkald78@gmail.com>
2021-04-30 20:47:34 +0300

    latest

---
+++ lib/Screens/HomePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce123/commoncode/const.dart';
+class HomePage extends StatelessWidget {
+  static String id = 'HomePage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: KmainColor,
+    );
+  }
+}
